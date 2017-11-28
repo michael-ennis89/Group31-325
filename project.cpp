c4f0c9759f3c6fb755f44febaa111f59b2849b9d
michael-ennis89 <ennism@oregonstate.edu>
2017-11-27 20:01:33 -0700

    Updated with NNA starting from city 0

--- project.cpp
+++ project.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <math.h>
+#include <climits>
 
 struct city{
 public:
@@ -31,9 +33,16 @@ void graph::addCity(int cityName, int x_cord, int y_cord)
 }
 
 
+//Function Prototypes
+
+int nearestNeighbor(graph G, int *path);
+
+
 int main(int argc, char* argv[])
 {
     graph myGraph;
+    int distance;
+    int *shortestPath;
 
     // Change to 3 with Usage <filename> <option> when implemented.
     /*if(argc != 2)
@@ -54,10 +63,75 @@ int main(int argc, char* argv[])
     }
 
     // Example to print out created graph.
-    for(i = 0; i < myGraph.cities; i++)
+    /*for(i = 0; i < myGraph.cities; i++)
     {
         std::cout << "City: " << myGraph.graphCity[i]->name << " x: " << myGraph.graphCity[i]->x << " y: " << myGraph.graphCity[i]->y << std::endl;
-    }
+    }*/
+    shortestPath = new int[myGraph.cities];
+    distance = nearestNeighbor(myGraph, shortestPath);
+
+    // un-comment to print distance afterwards.
+    std::cout << "Distance: " << distance << std::endl;
+    // un-comment to print path afterwards.
+    //for(i = 0; i < myGraph.cities; i++)
+        //std::cout << shortestPath[i] << std::endl;
 
     return 0;
+}
+
+int nearestNeighbor(graph G, int *path)
+{
+    int currentDistance = 0;
+    int roundDistance;
+    int roundCity;
+    int index = 0;
+    int i, j;
+    double x, y, dist;
+    city *currentCity;
+    path[index] = G.graphCity[index]->name;
+    G.graphCity[index]->visited = true;
+    currentCity = G.graphCity[index];
+    for(j = 0; j < G.cities; j++)
+    {
+        roundDistance = INT_MAX;
+        for(i = 0; i < G.cities; i++)
+        {
+            if(G.graphCity[i]->visited == false)
+            {
+                if(currentCity->name != G.graphCity[i]->name)
+                {
+                    x = (currentCity->x - G.graphCity[i]->x);
+                    y = (currentCity->y - G.graphCity[i]->y);
+                    dist = pow(x, 2) + pow(y, 2);
+                    dist = sqrt(dist);
+                    dist = fabs(dist);
+                    if(dist < roundDistance)
+                    {
+                        roundDistance = round(dist);
+                        roundCity = i;
+                    }
+                }
+            }
+        }
+        if(j < G.cities-1)  // otherwise prints distance from last node to nowhere.
+        {
+            index++;
+            path[index] = G.graphCity[roundCity]->name;
+            currentDistance += roundDistance;
+            currentCity = G.graphCity[roundCity];
+            G.graphCity[roundCity]->visited = true;
+            //un-comment if you want to see distances as you go.
+            //std::cout << "Current Distance: " << currentDistance << std::endl;
+        }
+    }
+    // distance last city to start
+    x = (currentCity->x - G.graphCity[0]->x);
+    y = (currentCity->y - G.graphCity[0]->y);
+    dist = pow(x, 2) + pow(y, 2);
+    dist = sqrt(dist);
+    dist = fabs(dist);
+    roundDistance = round(dist);
+    currentDistance += roundDistance;
+    path[G.cities] = G.graphCity[0]->name;
+    return currentDistance;
 }
